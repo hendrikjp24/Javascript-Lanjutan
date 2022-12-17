cf46de98183a9d59e81f051dae918e40aba563d3
hendrikjp24 <hendrikzerojp@gmail.com>
2022-12-17 20:19:27 +0700

    Mempelajari tentang Tagged Template Literals. Apa maksudnya serta contoh penggunaannya serta manfaat dari menggunakan Tagged Template Literals

---
+++ 5.3 Tagged Template Literals/script.js
@@ -0,0 +1,53 @@
+// Tagged Template Literals
+
+const nama = "Hendrik Ari Purna Irawan";
+const umur = 23;
+
+
+// string => menyimpan seluruh string yang didalam tanda back tick dan dipecahkandengan pemisahnya ${}
+
+// sedangkan ...values => bertindak sebagai rest args or value. jadi seluruh isi dari ${} akan ditampung dalam bentuk array
+function coba(strings, ...values){
+    return strings.reduce((result, str, i) => `${result}${str}${values[i] || ''}`, '');
+
+}
+
+// or diatas (||) dipakai sebagai pengganti ternary operator.
+// jadi, jika values dengan index i tidak ada, maka akan menjadi '', tetapi jika ada maka isi dari values akan ditampilkan
+
+// berhubung reduce yang sekarang digunakan untuk tipe data string, maka nilai awalnya kita set juga menjadi '', karena tidak perhitungan
+
+
+
+const Tampil = coba`Halo, Nama saya ${nama}, Umur saya ${umur} Tahun`;
+
+
+console.log(Tampil);
+
+
+
+
+
+// cth 2 Tagged Template Literals
+
+
+const namaMahasiswa = "Fathiya Al-Khaira";
+
+const umurMahasiswa = 27;
+
+const email = "Fathiyaalkhaira27@gmail.com";
+
+
+
+function showResult(strings, ...values){
+    return strings.reduce((resultValue, str, i) => `${resultValue}${str} <span class="higlight">${values[i] || ''}</span>`,'');
+
+}
+
+
+
+const result =  showResult`Halo, Nama Saya ${namaMahasiswa}, Umur Saya ${umurMahasiswa} Tahun dan email saya ${email}`;
+
+
+
+document.body.innerHTML = result;
